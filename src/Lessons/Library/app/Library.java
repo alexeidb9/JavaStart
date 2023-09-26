72419d0ecb866298e5f396d9f1577fdb58845651
Alexei A <alexey.alexandrenko@gmail.com>
2023-09-26 16:41:29 +0200

    Library v0.7 - implemented array

--- src/Lessons/Library/app/Library.java
+++ src/Lessons/Library/app/Library.java
@@ -4,17 +4,20 @@ import Lessons.Library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Library v0.7";
 
-        Book book1 = new Book("Hustle Harder","50 cent", 2021, 205, "Pinguin", "12345667889");
-        Book book2 = new Book("Letting Go", "David Hawkins", 1998, 225, "Pinguin", "12345667889");
-        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
+        Book[] book = new Book[1000];
+
+
+        book[0] = new Book("Hustle Harder","50 cent", 2021, 205, "Pinguin", "12345667889");
+        book[1] = new Book("Letting Go", "David Hawkins", 1998, 225, "Pinguin", "12345667889");
+        book[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
                 2008, 851, "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Books available in the library: ");
-        book1.printInfo();
-        book3.printInfo();
+        book[0].printInfo();
+        book[2].printInfo();
 
 
     }
