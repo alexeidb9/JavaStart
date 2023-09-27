def3714062191d838c9da83e1e59ddf99f67d5e5
Alexei A <alexey.alexandrenko@gmail.com>
2023-09-27 15:31:29 +0200

    Calculator v0.4 - interactive input - code refactoring

---
+++ src/Lessons/Calculator/app/CalcApp.java
@@ -0,0 +1,18 @@
+package Lessons.Calculator.app;
+
+import Lessons.Calculator.calc.Calculator;
+
+public class CalcApp {
+
+    public static void main(String[] args) {
+
+        String app = "Calculator v0.4";
+
+        Calculator calculator = new Calculator();
+        calculator.start();
+
+
+    }
+
+
+}
--- src/Lessons/Calculator/calc/Calculator.java
+++ src/Lessons/Calculator/calc/Calculator.java
@@ -1,9 +1,28 @@
-package Lessons.Calculator;
+package Lessons.Calculator.calc;
 
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Calculator {
+
+
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        System.out.println("Type in first number: ");
+        double a = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Provide an operation you want to perform: (+, -, /, *)");
+        String operator = sc.nextLine();
+        System.out.println("Type in another number: ");
+        double b = sc.nextDouble();
+
+        System.out.println(calculate(operator,a,b));
+
+        sc.close();
+    }
 
 
     public double calculate(String operator, double... numbers) {
